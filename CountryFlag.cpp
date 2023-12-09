a72f250704b22926eac8d221e7c4d0b81f8d705b
marc2k3 <marc2003@live.co.uk>
2023-12-09 13:10:18 +0000

    Add CountryFlag

---
+++ CountryFlag.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.hpp"
+#include "CountryFlag.hpp"
+
+std::string CountryFlag::get(wil::zstring_view country_or_code)
+{
+	init_flag_data();
+
+	if (country_or_code.length() == 2)
+	{
+		const auto lowered = string8(country_or_code).toLower();
+		const auto it = s_flag_data.find(lowered.get_ptr());
+
+		if (it != s_flag_data.end())
+		{
+			return it->second.flag;
+		}
+	}
+	else if (country_or_code.length() >= 4)
+	{
+		const auto it = std::ranges::find_if(s_flag_data, [&](auto&& item)
+			{
+				return compare_string(country_or_code, item.second.name);
+			});
+
+		if (it != s_flag_data.end())
+		{
+			return it->second.flag;
+		}
+	}
+
+	return std::string();
+}
+
+std::wstring CountryFlag::get(wil::zwstring_view country_or_code)
+{
+	const auto ucountry_or_code = from_wide(country_or_code);
+	const auto str = get(ucountry_or_code);
+	return to_wide(str);
+}
+
+void CountryFlag::init_flag_data()
+{
+	if (s_flag_data.empty())
+	{
+		const auto str = Component::get_resource_text(IDR_COUNTRIES_JSON);
+		const auto j = JSONHelper::parse(str);
+
+		for (auto&& item : j.items())
+		{
+			auto& value = item.value();
+
+			Country country;
+			country.name = value["name"].get<std::string>();
+			country.flag = value["flag"].get<std::string>();
+
+			s_flag_data.emplace(item.key(), country);
+		}
+	}
+}
---
+++ CountryFlag.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+class CountryFlag
+{
+public:
+	static std::string get(wil::zstring_view country_or_code);
+	static std::wstring get(wil::zwstring_view country_or_code);
+
+private:
+	struct Country
+	{
+		std::string name, flag;
+	};
+
+	using FlagData = std::map<std::string, Country>;
+
+	static void init_flag_data();
+
+	inline static FlagData s_flag_data;
+};
