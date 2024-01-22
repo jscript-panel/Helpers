5e4122fde0c43cfac42e68156b19fbb13b00fdb7
marc2k3 <marc2003@live.co.uk>
2024-01-22 23:37:35 +0000

    Update GdiScope.hpp

--- GdiScope.hpp
+++ GdiScope.hpp
@@ -1,22 +1,25 @@
 #pragma once
 
-struct GdiScope
+class GdiScope
 {
-	GdiScope(ID2D1RenderTarget* render_target)
+public:
+	GdiScope(ID2D1DeviceContext* context)
 	{
-		if SUCCEEDED(render_target->QueryInterface(IID_PPV_ARGS(&gdi)))
-		{
-			gdi->GetDC(D2D1_DC_INITIALIZE_MODE_COPY, &dc);
-		}
+		if FAILED(context->QueryInterface(IID_PPV_ARGS(&gdi))) return;
+		if FAILED(gdi->GetDC(D2D1_DC_INITIALIZE_MODE_COPY, &dc)) return;
+		is_valid = true;
 	}
 
 	~GdiScope()
 	{
-		if (gdi) gdi->ReleaseDC(nullptr);
+		if (is_valid) gdi->ReleaseDC(nullptr);
 	}
 
-	operator bool() const { return dc != nullptr; }
+	operator bool() const { return is_valid; }
 
 	HDC dc{};
+
+private:
+	bool is_valid{};
 	wil::com_ptr_t<ID2D1GdiInteropRenderTarget> gdi;
 };
