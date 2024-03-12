52122fa2f62002e24faf9b6d6cc4412b4ec70d1a
marc2k3 <marc2003@live.co.uk>
2024-03-12 16:07:14 +0000

    Update StringHelpers.hpp

--- StringHelpers.hpp
+++ StringHelpers.hpp
@@ -6,7 +6,11 @@ using Strings = std::vector<std::string>;
 using WStrings = std::vector<std::wstring>;
 using pfc::string8;
 
-static constexpr wil::zstring_view CRLF = "\r\n";
+inline pfc::string_base& operator<<(pfc::string_base& fmt, const std::string& source)
+{
+	fmt.add_string_(source.c_str());
+	return fmt;
+}
 
 BSTR to_bstr(std::string_view str);
 Strings split_string(std::string_view text, std::string_view delims);
@@ -17,3 +21,5 @@ std::string print_guid(const GUID& g);
 std::wstring to_wide(std::string_view str);
 std::wstring wdisplay_path(wil::zstring_view path);
 void sort_strings(WStrings& strings);
+
+static constexpr wil::zstring_view CRLF = "\r\n";
