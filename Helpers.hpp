b6e74e9132f484a56c584465377ea4b9dc01b455
marc2k3 <marc2003@live.co.uk>
2024-04-27 16:50:02 +0100

    Update pfc helpers

--- Helpers.hpp
+++ Helpers.hpp
@@ -23,11 +23,17 @@ namespace js
 	concept IsNum = std::integral<T> || std::floating_point<T>;
 
 	template <typename T>
-	static pfc::array_t<T> pfc_array(size_t count)
+	static auto pfc_array(size_t count)
 	{
 		pfc::array_t<T> arr;
 		arr.set_size(count);
 		return arr;
+	}
+
+	template <typename T>
+	static auto pfc_list(const T& item)
+	{
+		return pfc::list_single_ref_t<T>(item);
 	}
 
 	static void** arg_helper(auto arg)
