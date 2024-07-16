1f98ebae3cd3fd4042ea8913757d3c9b83aab7f3
marc2k3 <marc2003@live.co.uk>
2024-07-16 14:19:53 +0100

    Split helpers in to new files

---
+++ Colour.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+namespace js
+{
+	static int hex_digit_to_int(int ch)
+	{
+		if (ch >= '0' && ch <= '9')
+		{
+			return ch - '0';
+		}
+		else if (ch >= 'A' && ch <= 'F')
+		{
+			return ch - 'A' + 10;
+		}
+		else if (ch >= 'a' && ch <= 'f')
+		{
+			return ch - 'a' + 10;
+		}
+		return 0;
+	}
+
+	static int to_argb(COLORREF colour)
+	{
+		const auto c = GetRValue(colour) << RED_SHIFT | GetGValue(colour) << GREEN_SHIFT | GetBValue(colour) << BLUE_SHIFT | 0xff000000;
+		return to_int(c);
+	}
+
+	static COLORREF to_colorref(int colour)
+	{
+		return RGB(colour >> RED_SHIFT, colour >> GREEN_SHIFT, colour >> BLUE_SHIFT);
+	}
+
+	static D2D1_COLOR_F to_colorf(int64_t colour)
+	{
+		const auto a = to_float((colour >> 24) & 0xff) / 255.f;
+		return D2D1::ColorF(to_uint(colour), a);
+	}
+
+	static D2D1_COLOR_F to_colorf(JSON& colour)
+	{
+		return to_colorf(colour.get<int64_t>());
+	}
+
+	static D2D1_COLOR_F to_colorf(VARIANT colour)
+	{
+		if FAILED(VariantChangeType(&colour, &colour, 0, VT_I8))
+		{
+			return factory::White;
+		}
+
+		return to_colorf(colour.llVal);
+	}
+
+	static D2D1_COLOR_F to_colorf(std::wstring_view hex)
+	{
+		const int r = hex_digit_to_int(hex.at(4)) << 4 | hex_digit_to_int(hex.at(5));
+		const int g = hex_digit_to_int(hex.at(2)) << 4 | hex_digit_to_int(hex.at(3));
+		const int b = hex_digit_to_int(hex.at(0)) << 4 | hex_digit_to_int(hex.at(1));
+		const auto colour = to_argb(RGB(r, g, b));
+		return to_colorf(colour);
+	}
+}
---
+++ Font.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+struct Font
+{
+	Font() {}
+
+	Font(std::wstring_view name, float size, uint32_t weight, uint32_t style, uint32_t stretch)
+		: m_name(name)
+		, m_size(size)
+		, m_weight(static_cast<DWRITE_FONT_WEIGHT>(weight))
+		, m_style(static_cast<DWRITE_FONT_STYLE>(style))
+		, m_stretch(static_cast<DWRITE_FONT_STRETCH>(stretch)) {}
+
+	Font(JSON& obj)
+	{
+		if (!obj.is_object()) return;
+
+		auto name = js::json_to_wstring(obj["Name"]);
+		if (name.length())
+		{
+			m_name = name;
+		}
+
+		auto& size = obj["Size"];
+		if (size.is_number())
+		{
+			m_size = get<float>(size);
+		}
+
+		auto& weight = obj["Weight"];
+		if (weight.is_number_unsigned())
+		{
+			m_weight = get<DWRITE_FONT_WEIGHT>(weight);
+		}
+
+		auto& style = obj["Style"];
+		if (style.is_number_unsigned())
+		{
+			m_style = get<DWRITE_FONT_STYLE>(style);
+		}
+
+		auto& stretch = obj["Stretch"];
+		if (stretch.is_number_unsigned())
+		{
+			m_stretch = get<DWRITE_FONT_STRETCH>(stretch);
+		}
+
+		auto& strikethrough = obj["Strikethrough"];
+		if (strikethrough.is_boolean())
+		{
+			m_strikethrough = get_BOOL(strikethrough);
+		}
+
+		auto& underline = obj["Underline"];
+		if (underline.is_boolean())
+		{
+			m_underline = get_BOOL(underline);
+		}
+	}
+
+	Font(std::wstring_view str)
+	{
+		const auto parts = js::split_string(str, TAB);
+		if (parts.size() != 6) return;
+
+		m_name = parts[0];
+		m_size = get<float>(parts[1]);
+		m_weight = get<DWRITE_FONT_WEIGHT>(parts[2]);
+		m_style = get<DWRITE_FONT_STYLE>(parts[3]);
+
+		if (parts[4] == L"1") m_underline = TRUE;
+		if (parts[5] == L"1") m_strikethrough = TRUE;
+	}
+
+	static BOOL get_BOOL(JSON& obj)
+	{
+		return obj.get<bool>() ? TRUE : FALSE;
+	}
+
+	template <typename T>
+	static T get(JSON& obj)
+	{
+		return static_cast<T>(obj.get<uint32_t>());
+	}
+
+	template <typename T>
+	static T get(std::wstring_view str)
+	{
+		return static_cast<T>(std::stoul(str.data()));
+	}
+
+	BOOL m_strikethrough{}, m_underline{};
+	DWRITE_FONT_WEIGHT m_weight = DWRITE_FONT_WEIGHT_NORMAL;
+	DWRITE_FONT_STYLE m_style = DWRITE_FONT_STYLE_NORMAL;
+	DWRITE_FONT_STRETCH m_stretch = DWRITE_FONT_STRETCH_NORMAL;
+	float m_size = 16.f;
+	std::wstring m_name = factory::DefaultFont.data();
+};
