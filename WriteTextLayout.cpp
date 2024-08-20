d533299bf229d4645dd6173248b195c421939a2c
marc2k3 <marc2003@live.co.uk>
2024-08-20 03:34:57 +0100

    Rework WriteTextLayout

--- WriteTextLayout.cpp
+++ WriteTextLayout.cpp
@@ -1,127 +1,68 @@
 #include "stdafx.hpp"
 #include "WriteTextLayout.hpp"
 
-HRESULT WriteTextLayout::to_range(JSON& obj, DWRITE_TEXT_RANGE& range, bool verify_colour)
+WriteTextLayout::ColourRanges WriteTextLayout::parse_tf_colours(std::wstring_view text)
 {
-	if (obj.is_object() && obj["Start"].is_number_unsigned() && obj["Length"].is_number_unsigned())
+	ColourRanges colour_ranges;
+	const auto clean = js::remove_mark(text.data(), 7);
+	auto start = js::to_uint(clean.find(ETX));
+
+	if (start != std::wstring_view::npos)
 	{
-		RETURN_HR_IF(E_INVALIDARG, verify_colour && !obj["Colour"].is_number());
+		const auto parts = js::split_string(clean, ETX);
 
-		range.startPosition = obj["Start"].get<uint32_t>();
-		range.length = obj["Length"].get<uint32_t>();
-		return S_OK;
-	}
-
-	return E_INVALIDARG;
-}
-
-HRESULT WriteTextLayout::apply_colour(IDWriteTextLayout* text_layout, ID2D1DeviceContext* context, const D2D1_COLOR_F& colour, const DWRITE_TEXT_RANGE& range)
-{
-	wil::com_ptr_t<ID2D1SolidColorBrush> brush;
-	RETURN_IF_FAILED(context->CreateSolidColorBrush(colour, &brush));
-	RETURN_IF_FAILED(text_layout->SetDrawingEffect(brush.get(), range));
-	return S_OK;
-}
-
-HRESULT WriteTextLayout::apply_colours(IDWriteTextLayout* text_layout, ID2D1DeviceContext* context, JSON& jcolours)
-{
-	RETURN_HR_IF(E_INVALIDARG, !jcolours.is_array());
-
-	DWRITE_TEXT_RANGE range{};
-
-	for (auto&& jcolour : jcolours)
-	{
-		RETURN_IF_FAILED(to_range(jcolour, range, true));
-
-		const auto colour = js::to_colorf(jcolour["Colour"]);
-		RETURN_IF_FAILED(apply_colour(text_layout, context, colour, range));
-	}
-
-	return S_OK;
-}
-
-HRESULT WriteTextLayout::apply_colours(IDWriteTextLayout* text_layout, ID2D1DeviceContext* context, std::wstring_view text)
-{
-	uint32_t start = js::to_uint(text.find(ETX));
-	const auto parts = js::split_string(text, ETX);
-
-	for (size_t i = 1; i < parts.size(); i += 2)
-	{
-		const auto& colour_part = parts[i];
-		const auto& text_part = parts[i + 1];
-		if (text_part.empty()) continue;
-
-		const auto range = DWRITE_TEXT_RANGE(start, js::lengthu(text_part));
-		start += range.length;
-
-		if (!colour_part.empty())
+		for (size_t i = 1; i < parts.size(); i += 2)
 		{
-			RETURN_IF_FAILED(apply_colour(text_layout, context, js::to_colorf(colour_part), range));
+			const auto& colour_part = parts[i];
+			const auto& text_part = parts[i + 1];
+			if (text_part.empty()) continue;
+
+			const auto range = DWRITE_TEXT_RANGE(start, js::lengthu(text_part));
+			start += range.length;
+
+			if (!colour_part.empty())
+			{
+				ColourRange colour_range{};
+				colour_range.colour = js::to_colorf(colour_part);
+				colour_range.range = range;
+				colour_ranges.emplace_back(colour_range);
+			}
 		}
 	}
 
-	return S_OK;
+	return colour_ranges;
 }
 
-HRESULT WriteTextLayout::apply_font(IDWriteTextLayout* text_layout, const Font& font, const DWRITE_TEXT_RANGE& range)
+WriteTextLayout::FontRanges WriteTextLayout::parse_tf_fonts(std::wstring_view text)
 {
-	RETURN_IF_FAILED(text_layout->SetFontFamilyName(font.m_name.data(), range));
-	RETURN_IF_FAILED(text_layout->SetFontSize(font.m_size, range));
-	RETURN_IF_FAILED(text_layout->SetFontWeight(font.m_weight, range));
-	RETURN_IF_FAILED(text_layout->SetFontStyle(font.m_style, range));
-	RETURN_IF_FAILED(text_layout->SetFontStretch(font.m_stretch, range));
-	RETURN_IF_FAILED(text_layout->SetStrikethrough(font.m_strikethrough, range));
-	RETURN_IF_FAILED(text_layout->SetUnderline(font.m_underline, range));
-	return S_OK;
-}
+	FontRanges font_ranges;
+	const auto clean = js::remove_mark(text.data(), 3);
+	auto start = js::to_uint(clean.find(BEL));
 
-HRESULT WriteTextLayout::apply_fonts(IDWriteTextLayout* text_layout, JSON& jfonts)
-{
-	for (auto&& jfont : jfonts)
+	if (start != std::wstring_view::npos)
 	{
-		DWRITE_TEXT_RANGE range{};
-		const auto font = Font(jfont);
-		RETURN_IF_FAILED(to_range(jfont, range));
-		RETURN_IF_FAILED(apply_font(text_layout, font, range));
-	}
+		const auto parts = js::split_string(clean, BEL);
 
-	return S_OK;
-}
-
-HRESULT WriteTextLayout::apply_fonts(IDWriteTextLayout* text_layout, std::wstring_view text)
-{
-	uint32_t start = js::to_uint(text.find(BEL));
-	const auto parts = js::split_string(text, BEL);
-
-	for (size_t i = 1; i < parts.size(); i += 2)
-	{
-		const auto& font_part = parts[i];
-		const auto& text_part = parts[i + 1];
-		if (text_part.empty()) continue;
-
-		const auto range = DWRITE_TEXT_RANGE(start, js::lengthu(text_part));
-		start += range.length;
-
-		if (!font_part.empty())
+		for (size_t i = 1; i < parts.size(); i += 2)
 		{
-			const auto font = Font(font_part);
-			RETURN_IF_FAILED(apply_font(text_layout, font, range));
+			const auto& font_part = parts[i];
+			const auto& text_part = parts[i + 1];
+			if (text_part.empty()) continue;
+
+			const auto range = DWRITE_TEXT_RANGE(start, js::lengthu(text_part));
+			start += range.length;
+
+			if (!font_part.empty())
+			{
+				FontRange font_range{};
+				font_range.font = Font(font_part);
+				font_range.range = range;
+				font_ranges.emplace_back(font_range);
+			}
 		}
 	}
 
-	return S_OK;
-}
-
-HRESULT WriteTextLayout::apply_line_spacing(IDWriteTextLayout3* text_layout)
-{
-	DWRITE_LINE_SPACING spacing{};
-	spacing.method = DWRITE_LINE_SPACING_METHOD_PROPORTIONAL;
-	spacing.height = 1.f;
-	spacing.baseline = 1.f;
-	spacing.leadingBefore = 0.f;
-	spacing.fontLineGapUsage = DWRITE_FONT_LINE_GAP_USAGE_DISABLED;
-
-	return text_layout->SetLineSpacing(&spacing);
+	return font_ranges;
 }
 
 HRESULT WriteTextLayout::create(wil::com_ptr_t<IDWriteTextLayout>& text_layout, const Font& font, const FormatParams& params, std::wstring_view text, float width, float height)
@@ -146,4 +87,87 @@ HRESULT WriteTextLayout::create3(wil::com_ptr_t<IDWriteTextLayout3>& text_layout
 	RETURN_IF_FAILED(create(temp_layout, font, params, text, FLT_MAX, FLT_MAX));
 	RETURN_HR_IF(E_FAIL, !temp_layout.try_query_to(IID_PPV_ARGS(&text_layout)));
 	return S_OK;
+}
+
+HRESULT WriteTextLayout::set_colours(IDWriteTextLayout* text_layout, ID2D1DeviceContext* context, const ColourRanges& colour_ranges)
+{
+	for (auto&& colour_range : colour_ranges)
+	{
+		wil::com_ptr_t<ID2D1SolidColorBrush> brush;
+		RETURN_IF_FAILED(context->CreateSolidColorBrush(colour_range.colour, &brush));
+		RETURN_IF_FAILED(text_layout->SetDrawingEffect(brush.get(), colour_range.range));
+	}
+
+	return S_OK;
+}
+
+HRESULT WriteTextLayout::set_colours_json(IDWriteTextLayout* text_layout, ID2D1DeviceContext* context, JSON& jcolours)
+{
+	ColourRanges colour_ranges(jcolours.size());
+
+	for (auto&& [colour_range, jcolour] : std::views::zip(colour_ranges, jcolours))
+	{
+		RETURN_IF_FAILED(to_range(jcolour, colour_range.range, true));
+		colour_range.colour = js::to_colorf(jcolour["Colour"]);
+	}
+
+	return set_colours(text_layout, context, colour_ranges);
+}
+
+HRESULT WriteTextLayout::set_fonts(IDWriteTextLayout* text_layout, const FontRanges& font_ranges)
+{
+	for (auto&& font_range : font_ranges)
+	{
+		RETURN_IF_FAILED(text_layout->SetFontFamilyName(font_range.font.m_name.data(), font_range.range));
+		RETURN_IF_FAILED(text_layout->SetFontSize(font_range.font.m_size, font_range.range));
+		RETURN_IF_FAILED(text_layout->SetFontWeight(font_range.font.m_weight, font_range.range));
+		RETURN_IF_FAILED(text_layout->SetFontStyle(font_range.font.m_style, font_range.range));
+		RETURN_IF_FAILED(text_layout->SetFontStretch(font_range.font.m_stretch, font_range.range));
+		RETURN_IF_FAILED(text_layout->SetStrikethrough(font_range.font.m_strikethrough, font_range.range));
+		RETURN_IF_FAILED(text_layout->SetUnderline(font_range.font.m_underline, font_range.range));
+	}
+
+	return S_OK;
+}
+
+HRESULT WriteTextLayout::set_fonts_json(IDWriteTextLayout* text_layout, JSON& jfonts)
+{
+	if (!jfonts.is_array())
+		return S_OK;
+
+	FontRanges font_ranges(jfonts.size());
+
+	for (auto&& [font_range, jfont] : std::views::zip(font_ranges, jfonts))
+	{
+		RETURN_IF_FAILED(to_range(jfont, font_range.range));
+		font_range.font = Font(jfont);
+	}
+
+	return set_fonts(text_layout, font_ranges);
+}
+
+HRESULT WriteTextLayout::set_line_spacing(IDWriteTextLayout3* text_layout)
+{
+	DWRITE_LINE_SPACING spacing{};
+	spacing.method = DWRITE_LINE_SPACING_METHOD_PROPORTIONAL;
+	spacing.height = 1.f;
+	spacing.baseline = 1.f;
+	spacing.leadingBefore = 0.f;
+	spacing.fontLineGapUsage = DWRITE_FONT_LINE_GAP_USAGE_DISABLED;
+
+	return text_layout->SetLineSpacing(&spacing);
+}
+
+HRESULT WriteTextLayout::to_range(JSON& obj, DWRITE_TEXT_RANGE& range, bool verify_colour)
+{
+	if (obj.is_object() && obj["Start"].is_number_unsigned() && obj["Length"].is_number_unsigned())
+	{
+		RETURN_HR_IF(E_INVALIDARG, verify_colour && !obj["Colour"].is_number());
+
+		range.startPosition = obj["Start"].get<uint32_t>();
+		range.length = obj["Length"].get<uint32_t>();
+		return S_OK;
+	}
+
+	return E_INVALIDARG;
 }
--- WriteTextLayout.hpp
+++ WriteTextLayout.hpp
@@ -4,16 +4,31 @@
 class WriteTextLayout
 {
 public:
-	static HRESULT apply_colours(IDWriteTextLayout* text_layout, ID2D1DeviceContext* context, JSON& jcolours);
-	static HRESULT apply_colours(IDWriteTextLayout* text_layout, ID2D1DeviceContext* context, std::wstring_view text);
-	static HRESULT apply_fonts(IDWriteTextLayout* text_layout, JSON& jfonts);
-	static HRESULT apply_fonts(IDWriteTextLayout* text_layout, std::wstring_view text);
-	static HRESULT apply_line_spacing(IDWriteTextLayout3* text_layout);
+	struct ColourRange
+	{
+		D2D1_COLOR_F colour;
+		DWRITE_TEXT_RANGE range;
+	};
+
+	struct FontRange
+	{
+		Font font;
+		DWRITE_TEXT_RANGE range;
+	};
+
+	using ColourRanges = std::vector<ColourRange>;
+	using FontRanges = std::vector<FontRange>;
+
+	static ColourRanges parse_tf_colours(std::wstring_view text);
+	static FontRanges parse_tf_fonts(std::wstring_view text);
 	static HRESULT create(wil::com_ptr_t<IDWriteTextLayout>& text_layout, const Font& font, const FormatParams& params, std::wstring_view text, float width, float height);
 	static HRESULT create3(wil::com_ptr_t<IDWriteTextLayout3>& text_layout, const Font& font, const FormatParams& params, std::wstring_view text);
+	static HRESULT set_colours(IDWriteTextLayout* text_layout, ID2D1DeviceContext* context, const ColourRanges& colour_ranges);
+	static HRESULT set_colours_json(IDWriteTextLayout* text_layout, ID2D1DeviceContext* context, JSON& jcolours);
+	static HRESULT set_fonts(IDWriteTextLayout* text_layout, const FontRanges& font_ranges);
+	static HRESULT set_fonts_json(IDWriteTextLayout* text_layout, JSON& jfonts);
+	static HRESULT set_line_spacing(IDWriteTextLayout3* text_layout);
 
 private:
 	static HRESULT to_range(JSON& obj, DWRITE_TEXT_RANGE& range, bool verify_colour = false);
-	static HRESULT apply_colour(IDWriteTextLayout* text_layout, ID2D1DeviceContext* context, const D2D1_COLOR_F& colour, const DWRITE_TEXT_RANGE& range);
-	static HRESULT apply_font(IDWriteTextLayout* text_layout, const Font& font, const DWRITE_TEXT_RANGE& range);
 };
